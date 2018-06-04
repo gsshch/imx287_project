feb1f97d8f1ef6c263d255b277c5bb0dd364611f
gsshch <1729719432@qq.com>
2018-06-04 14:24:40 +0800

    第一次添加linux2.6到github;

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Mon Jun 4 14:21:56 CST 2018"
+#define LINUX_COMPILE_TIME "14:21:56"
+#define LINUX_COMPILE_BY "guo"
+#define LINUX_COMPILE_HOST "guo-ubuntu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.4 (4.4.4_09.06.2010) "
